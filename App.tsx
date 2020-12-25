ed9ce22c0967efec74a1b981b8e266fb350c5d66
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2020-12-25 23:15:46 +0800

    Added real-time boot screen and bios screen classifier (Still very slow and has many errors)

--- App.tsx
+++ App.tsx
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Dimensions, Platform } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as tf from '@tensorflow/tfjs';
-import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
+import { bundleResourceIO, cameraWithTensors } from '@tensorflow/tfjs-react-native';
+import { GraphModel, Tensor } from '@tensorflow/tfjs';
+
+// Disable logs on EXPO client!
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs();//Ignore all log notifications
+console.log('STARTING');
 
 export default function App() {
   // Camera permissions
@@ -10,6 +16,7 @@ export default function App() {
 
   //  Tensor Camera
   const TensorCamera = cameraWithTensors(Camera);
+  let requestAnimationFrameID = 0;
 
   // Screen Ratio
   const { height, width } = Dimensions.get('window');
@@ -19,15 +26,20 @@ export default function App() {
   const cameraHeight = (4 / 3) * width;
 
   // Tensorflow and Permissions
-  // check model state here (not available yet)
+  // Check model state
+  const [model, setModel] = useState<GraphModel | null>(null);
   const [frameworkReady, setFrameworkReady] = useState(false);
 
   // Performance hacks (Platform dependent)
   const textureDims = Platform.OS === "ios"? { width: 1080, height: 1920 } : { width: 1600, height: 1200 };
-  const tensorDims = { width: 152, height: 200 };
+  const tensorDims = { width: 180, height: 180 };
 
+  const [prediction, setPrediction] = useState<String | undefined>('');
+
+  // -----------------------------
+  // Initialize
   useEffect(() => {
-    if(!frameworkReady) {
+    if (!frameworkReady) {
       (async () => {
         // Check camera permission
         const { status } = await Camera.requestPermissionsAsync();
@@ -36,23 +48,72 @@ export default function App() {
         // Wait for the Tensorflow API to be ready before any TF operation
         await tf.ready();
 
+        console.log('LOADING MODEL');
+        
         // Load the model and save it in state
-
+        setModel(await loadModel());
+        console.log('MODEL LOADED');
+        
 
         // Ready
         setFrameworkReady(true);
+        console.log('FRAMEWORK READY');
+        
       })();
     }
   }, []);
 
-  const handleCameraStream = (imageAsTensors: any) => {
+  useEffect(() => {
+    if (requestAnimationFrameID) {
+      cancelAnimationFrame(requestAnimationFrameID);
+    }
+  }, []);
+
+  const loadModel = async () => {
+    const modelJSON = await require('./assets/model/model.json');
+    const modelWeights = await require('./assets/model/group1-shard.bin');
+    const model = await tf.loadGraphModel(bundleResourceIO(modelJSON, modelWeights));
+    
+    return model;
+  }
+
+  const getPrediction = async (tensor: Tensor) => {
+    if (!tensor) { return '' }
+    if (!model) { return '' }
+
+    // Get probabilities
+    const output = model.predict(tensor.expandDims(0).toFloat()) as Tensor;
+    console.log(output.dataSync());
+    
+    const predictions = Array.from(output.argMax(1).dataSync())
+    console.log(predictions);
+
+    var className = '';
+
+    if (predictions[0] == 1)
+    {
+      className = 'BIOS_SCREEN'
+    }
+    else if (predictions[0] == 2)
+    {
+      className = 'BOOT_SCREEN'
+    }
+    else
+    {
+      className = 'NONE'
+    }
+    console.log(className);
+    
+    return className;
+  }
+
+  const handleCameraStream = (imageAsTensors: IterableIterator<tf.Tensor3D>) => {
     const loop = async () => {
       const nextImageTensor = await imageAsTensors.next().value;
-      //await getPrediction(nextImageTensor);
-      //requestAnimationFrameId = requestAnimationFrame(loop);
+      setPrediction(await getPrediction(nextImageTensor));
+      
       requestAnimationFrame(loop);
     };
-    //if(!predictionFound) loop();
     loop();
   }
 
@@ -61,6 +122,9 @@ export default function App() {
   }
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
+  }
+  if (model === null) {
+    return <Text>Model not loaded</Text>;
   }
   return (
     <View style={styles.container}>
@@ -77,6 +141,8 @@ export default function App() {
         onReady={(imageAsTensors) => handleCameraStream(imageAsTensors)}
         autorender={true}
       />
+      
+      <Text style={styles.text}>{prediction}</Text>
     </View>
   );
 }
@@ -84,12 +150,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'transparent',
-    flexDirection: 'row'
+    backgroundColor: 'transparent'
   },
   camera: {
-    flex: 1,
     marginTop: 50
   },
+  body: {
+    justifyContent: 'center',
+    marginBottom: 50
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#000000'
+  }
 });
