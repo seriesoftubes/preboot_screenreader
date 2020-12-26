827dfb08ac397cebb0765c827770f52ac1df35d0
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2020-12-26 17:41:11 +0800

    Change model load to before app is runnning and improve classification.

--- metro.config.js
+++ metro.config.js
@@ -4,8 +4,8 @@ module.exports = (async () => {
   const { assetExts } = defaultConfig.resolver;
   return {
     resolver: {
-      // Add bin to assetExts
       assetExts: [...assetExts, 'bin'],
+      sourceExts: ['js', 'json', 'ts', 'tsx', 'jsx'],
     }
   };
 })();
