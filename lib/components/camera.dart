50c020262701911da39ccb9fa6dd1ce1293af968
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2021-02-01 23:26:56 +0800

    Added firebase ml vision for text recognition

--- lib/components/camera.dart
+++ lib/components/camera.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:tflite/tflite.dart';
 
 import 'classifier.dart';
+import 'text_recognition.dart';
+import 'text_recognition_painter.dart';
 import 'text_to_speech.dart';
 
 class CameraFeed extends StatefulWidget {
@@ -13,9 +16,13 @@ class CameraFeed extends StatefulWidget {
 class _CameraFeedState extends State<CameraFeed> {
   Classifier _classifier;
   TextToSpeech _tts;
+  TextRecognition _tr;
   CameraController cameraController;
+  Size cameraPreviewSize;
   bool isDetecting = false;
   String _prediction = "None";
+  String prediction, lastPrediction;
+  VisionText visionText;
 
   Future initCamera(CameraDescription cameraDescription) async {
     if (cameraController != null) {
@@ -57,6 +64,23 @@ class _CameraFeedState extends State<CameraFeed> {
     );
   }
 
+  // Display text recognition painter preview
+  Widget textPainterPreview() {
+    CustomPainter painter;
+    if (visionText == null) {
+      return Container();
+    }
+    cameraPreviewSize = Size(
+      cameraController.value.previewSize.height,
+      cameraController.value.previewSize.width,
+    );
+    painter = TextRecognitionPainter(cameraPreviewSize, visionText);
+
+    return CustomPaint(
+      foregroundPainter: painter,
+    );
+  }
+
   // Display prediction text
   Widget predictionText() {
     return Text(
@@ -76,6 +100,7 @@ class _CameraFeedState extends State<CameraFeed> {
   void dispose() {
     Tflite.close();
     _tts.stop();
+    _tr.close();
     super.dispose();
     if (cameraController != null) {
       cameraController.stopImageStream();
@@ -95,6 +120,10 @@ class _CameraFeedState extends State<CameraFeed> {
             child: cameraPreview(),
           ),
           Align(
+            alignment: Alignment.center,
+            child: textPainterPreview(),
+          ),
+          Align(
             alignment: Alignment.bottomCenter,
             child: predictionText(),
           ),
@@ -107,6 +136,7 @@ class _CameraFeedState extends State<CameraFeed> {
   void _init() async {
     _classifier = Classifier(); // loads the model.
     _tts = TextToSpeech(); // initialize text to speech
+    _tr = TextRecognition(); // initialize text recognition
     try {
       final cameras = await availableCameras();
       if (cameras?.isNotEmpty ?? false) {
@@ -123,17 +153,25 @@ class _CameraFeedState extends State<CameraFeed> {
     if (isDetecting) return;
     isDetecting = true;
 
-    var prediction = await _classifier.classify(img);
+    prediction = await _classifier.classify(img);
+
+    if (lastPrediction == prediction) {
+      if (lastPrediction == "BIOS SCREEN") {
+        visionText = await _tr.recognizeText(img);
+      }
+    } else {
+      if (prediction == "BOOT SCREEN") {
+        _tts.speak("BOOT SCREEN. Press the key to enter BIOS screen.");
+      } else if (prediction == "BIOS SCREEN") {
+        _tts.speak("BIOS SCREEN");
+      }
+    }
 
     setState(() {
       _prediction = prediction;
     });
 
-    if (prediction == "BOOT SCREEN") {
-      _tts.speak("BOOT SCREEN. Press the key to enter BIOS screen.");
-    } else if (prediction == "BIOS SCREEN") {
-      _tts.speak("BIOS SCREEN");
-    }
+    lastPrediction = prediction;
 
     isDetecting = false;
   }
---
+++ lib/components/text_recognition.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';
+
+import 'package:camera/camera.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+
+class TextRecognition {
+  TextRecognizer textRecognizer;
+  FirebaseVisionImage visionImage;
+  VisionText visionText;
+
+  TextRecognition() {
+    textRecognizer = FirebaseVision.instance.textRecognizer();
+  }
+
+  Future<VisionText> recognizeText(CameraImage img) async {
+    visionImage = FirebaseVisionImage.fromBytes(
+        img.planes[0].bytes,
+        FirebaseVisionImageMetadata(
+          rawFormat: img.format.raw,
+          size: Size(img.width.toDouble(), img.height.toDouble()),
+          rotation: ImageRotation.rotation90,
+          planeData: img.planes.map((plane) {
+            return FirebaseVisionImagePlaneMetadata(
+              bytesPerRow: plane.bytesPerRow,
+              width: plane.width,
+              height: plane.height,
+            );
+          }).toList(),
+        ));
+    visionText = await textRecognizer.processImage(visionImage);
+    return visionText;
+  }
+
+  Future close() async {
+    await textRecognizer.close();
+  }
+}
